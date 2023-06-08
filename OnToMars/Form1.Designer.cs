3c98a8f537c94bf0a85f3138b93279b3e6b7f4b5
SteFan192 <105925310+SteFan192@users.noreply.github.com>
2023-06-08 21:21:00 +0200

    Add files via upload

---
+++ OnToMars/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace OnToMars
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbRaketa = new System.Windows.Forms.PictureBox();
+            this.pbMeteor = new System.Windows.Forms.PictureBox();
+            this.btStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbPoeni = new System.Windows.Forms.Label();
+            this.pbMeteor2 = new System.Windows.Forms.PictureBox();
+            this.pbMeteor3 = new System.Windows.Forms.PictureBox();
+            this.pbMars = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRaketa)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMeteor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMeteor2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMeteor3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMars)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbRaketa
+            // 
+            this.pbRaketa.BackColor = System.Drawing.Color.Transparent;
+            this.pbRaketa.Image = global::OnToMars.Properties.Resources.RaketaX;
+            this.pbRaketa.Location = new System.Drawing.Point(440, 630);
+            this.pbRaketa.Name = "pbRaketa";
+            this.pbRaketa.Size = new System.Drawing.Size(45, 100);
+            this.pbRaketa.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbRaketa.TabIndex = 2;
+            this.pbRaketa.TabStop = false;
+            // 
+            // pbMeteor
+            // 
+            this.pbMeteor.BackColor = System.Drawing.Color.Transparent;
+            this.pbMeteor.Image = global::OnToMars.Properties.Resources.Meteor;
+            this.pbMeteor.Location = new System.Drawing.Point(124, 28);
+            this.pbMeteor.Name = "pbMeteor";
+            this.pbMeteor.Size = new System.Drawing.Size(60, 120);
+            this.pbMeteor.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMeteor.TabIndex = 4;
+            this.pbMeteor.TabStop = false;
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(900, 720);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(75, 23);
+            this.btStart.TabIndex = 5;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(20, 730);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Poeni:";
+            this.label1.UseMnemonic = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 75;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbPoeni
+            // 
+            this.lbPoeni.AutoSize = true;
+            this.lbPoeni.BackColor = System.Drawing.Color.Transparent;
+            this.lbPoeni.ForeColor = System.Drawing.Color.White;
+            this.lbPoeni.Location = new System.Drawing.Point(64, 731);
+            this.lbPoeni.Name = "lbPoeni";
+            this.lbPoeni.Size = new System.Drawing.Size(0, 13);
+            this.lbPoeni.TabIndex = 7;
+            // 
+            // pbMeteor2
+            // 
+            this.pbMeteor2.Image = global::OnToMars.Properties.Resources.Meteor;
+            this.pbMeteor2.Location = new System.Drawing.Point(462, 82);
+            this.pbMeteor2.Name = "pbMeteor2";
+            this.pbMeteor2.Size = new System.Drawing.Size(60, 120);
+            this.pbMeteor2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMeteor2.TabIndex = 8;
+            this.pbMeteor2.TabStop = false;
+            // 
+            // pbMeteor3
+            // 
+            this.pbMeteor3.Image = global::OnToMars.Properties.Resources.Meteor;
+            this.pbMeteor3.Location = new System.Drawing.Point(736, 321);
+            this.pbMeteor3.Name = "pbMeteor3";
+            this.pbMeteor3.Size = new System.Drawing.Size(60, 120);
+            this.pbMeteor3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMeteor3.TabIndex = 9;
+            this.pbMeteor3.TabStop = false;
+            // 
+            // pbMars
+            // 
+            this.pbMars.Image = global::OnToMars.Properties.Resources.Mars;
+            this.pbMars.Location = new System.Drawing.Point(683, -3);
+            this.pbMars.Name = "pbMars";
+            this.pbMars.Size = new System.Drawing.Size(250, 250);
+            this.pbMars.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMars.TabIndex = 10;
+            this.pbMars.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 589);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 14);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Strelica levo - levo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 603);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 14);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Strelica desno - desno";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(12, 617);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 14);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Strelica gore - ubrzanje";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(12, 631);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(128, 14);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Strelica dole - usporavanje";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(984, 749);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pbMars);
+            this.Controls.Add(this.pbMeteor3);
+            this.Controls.Add(this.pbMeteor2);
+            this.Controls.Add(this.lbPoeni);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.pbMeteor);
+            this.Controls.Add(this.pbRaketa);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OnToMars";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pbRaketa)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMeteor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMeteor2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMeteor3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMars)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pbRaketa;
+        private System.Windows.Forms.PictureBox pbMeteor;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbPoeni;
+        private System.Windows.Forms.PictureBox pbMeteor2;
+        private System.Windows.Forms.PictureBox pbMeteor3;
+        private System.Windows.Forms.PictureBox pbMars;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ OnToMars/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OnToMars
+{
+    public partial class Form1 : Form
+    {
+
+        int brzinarakete;
+        int brzinapadanja;
+        bool levo;
+        bool desno;
+        int Poeni;
+        Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            Start();
+        }
+
+        private void Start()
+        {
+            btStart.Enabled = false;
+            levo = false;
+            desno = false;
+            Poeni = 0;
+            brzinapadanja = 15;
+            brzinarakete = 10;
+            pbRaketa.Left = 440;
+            pbRaketa.Top = 630;
+            pbMeteor.Left = r.Next(100, 333);
+            pbMeteor.Top = r.Next(0, 30);
+            pbMeteor2.Left = r.Next(333, 667);
+            pbMeteor2.Top = r.Next(0, 30);
+            pbMeteor3.Left = r.Next(667, 790);
+            pbMeteor3.Top = r.Next(0, 30);
+            pbMars.Left = 570;
+            pbMars.Top = 0;
+            timer1.Start();
+        }
+
+        private void Kraj()
+        {
+            timer1.Stop();
+            btStart.Enabled = true;
+            MessageBox.Show("Kraj igre, pokusajte ponovo!");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Poeni++;
+            lbPoeni.Text = Convert.ToString(Poeni);
+            pbMeteor.Top += brzinapadanja;
+            pbMeteor2.Top += brzinapadanja;
+            pbMeteor3.Top += brzinapadanja;
+            pbMars.Visible = false;
+
+            if (Poeni >= 300)
+            {
+                label2.Visible = false;
+                label3.Visible = false;
+                label4.Visible = false;
+                label5.Visible = false;
+            }
+
+            if (Poeni >= 100)
+            {
+                if (brzinapadanja < 20)
+                {
+                    brzinapadanja = 20;
+                }
+                if (brzinarakete < 20)
+                {
+                    brzinarakete = 18;
+                }
+            }
+            if (Poeni >= 300)
+            {
+                if (brzinapadanja < 40)
+                {
+                    brzinapadanja = 40;
+                }
+                if (brzinarakete < 35)
+                {
+                    brzinarakete = 25;
+                }
+            }
+            if (Poeni >= 500)
+            {
+                if (brzinapadanja < 60)
+                {
+                    brzinapadanja = 60;
+                }
+                if (brzinarakete < 40)
+                {
+                    brzinarakete = 40;
+                }
+            }
+
+            if (Poeni>=700)
+            {
+                timer1.Stop();
+                pbMars.Visible = true;
+                btStart.Enabled = true;
+                MessageBox.Show("Nakon dugog putovanja nasa raketa je konacno stigla na svoje odrediste, crvenu planetu Mars!");
+                MessageBox.Show("Igra je gotova, ako zelite da igrate igru ponovo pritisnite dugme Start.");
+            }
+
+            if(desno && pbRaketa.Left <= 800)
+            {
+                pbRaketa.Left += brzinarakete;
+            }
+            if(levo && pbRaketa.Left >= 100)
+            {
+                pbRaketa.Left -= brzinarakete;
+            }
+            
+
+            if(pbMeteor.Top > 730)
+            {
+                pbMeteor.Left = r.Next(100, 333);
+                pbMeteor.Top = r.Next(-110, 20);
+            }
+            if(pbMeteor2.Top > 730)
+            {
+                pbMeteor2.Left = r.Next(333, 667);
+                pbMeteor2.Top = r.Next(-110, 20);
+            }
+            if(pbMeteor3.Top > 730)
+            {
+                pbMeteor3.Left = r.Next(667, 790);
+                pbMeteor3.Top = r.Next(-110,20);
+            }
+            
+
+            if(pbRaketa.Bounds.IntersectsWith(pbMeteor.Bounds))
+            {
+                Kraj();
+            }
+            if(pbRaketa.Bounds.IntersectsWith(pbMeteor2.Bounds))
+            {
+                Kraj();
+            }
+            if(pbRaketa.Bounds.IntersectsWith(pbMeteor3.Bounds))
+            {
+                Kraj();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Right)
+            {
+                desno = true;
+            }
+            if(e.KeyCode==Keys.Left)
+            {
+                levo = true;
+            }
+            if(e.KeyCode==Keys.Up)
+            {
+                brzinapadanja += 3;
+                brzinarakete += 1;
+            }
+            if(e.KeyCode==Keys.Down)
+            {
+                if(brzinapadanja>=4)
+                {
+                    brzinapadanja -= 2;
+                }
+                if(brzinarakete>=4)
+                {
+                    brzinarakete -= 1;
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Right)
+            {
+                desno = false;
+            }
+            if(e.KeyCode == Keys.Left)
+            {
+                levo = false;
+            }
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            Start();
+        }
+    }
+}
